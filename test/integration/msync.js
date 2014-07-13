b821a10aecbd40dbb5e7084709686dbeb89fb5ed
Benjamin Bengfort <benjamin@bengfort.com>
2014-07-13 12:48:56 -0400

    test stubs for msync

---
+++ test/integration/msync.js
@@ -0,0 +1,47 @@
+/**
+ *  test/integration/msnc.js
+ *  Test for the msync application
+ *
+ *  Author:   Benjamin Bengfort <benb@datacommunitydc.org>
+ *  Created:  Sun Jul 13 12:45:55 2014 -0400
+ *
+ *  Copyright (C) 2014 Data Community DC
+ *  For license information, see LICENSE.txt
+ *
+ *  ID: msync.js [] benb@datacommunitydc.org $
+ */
+
+// JS Hint directives and strict mode
+/* globals exports,__filename */
+'use strict';
+
+module.exports = function() {
+
+  var expect = require('chai').expect
+    , _ = require('underscore')
+    , Q = require('q')
+    , mongoose = require('mongoose');
+
+  describe('msync Command Tests', function() {
+
+    before(function(done) {
+      done();
+    });
+
+    beforeEach(function(done) {
+      done();
+    });
+
+    afterEach(function(done) {
+      done();
+    });
+
+    after(function(done) {
+      done();
+    });
+
+    it('should have tests');
+
+  });
+
+};
